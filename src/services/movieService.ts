98de93ac9d08270301a521c69d6c5df25d1d105a
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-05-04 21:00:40 +0300

    init hw

---
+++ src/services/movieService.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import type { Movie, TMDBResponse } from "../types/movie";
+
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const config = {
+    params: {
+      query: query,
+      include_adult: false,
+      language: "uk-UA",
+    },
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      accept: "application/json",
+    },
+  };
+
+  const response = await axios.get<TMDBResponse>(
+    `${BASE_URL}/search/movie`,
+    config,
+  );
+  return response.data.results;
+};
---
+++ src/types/movie.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+  id: number;
+  poster_path: string;
+  backdrop_path: string;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+}
+
+export interface TMDBResponse {
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
